51c12b1e4e7d5a82c5b4dcdda177b8db3499a6fb
Edgar Andres <edgar.vargas@correounivalle.edu.co>
2023-12-10 17:32:00 -0500

    push

--- app/src/main/scala/Proyecto/Proyecto.scala
+++ app/src/main/scala/Proyecto/Proyecto.scala
@@ -1,129 +1,46 @@
-
-package Proyecto
-import common._
-import org.scalameter.{Warmer, withWarmer}
-import scala.collection.parallel.immutable.ParVector
-import scala.util.Random
-
-
-
-object proyecto {
-
- val alfabeto = Seq('a', 'c', 'g', 't')
-
- type Oraculo = Seq[Char] => Boolean
- abstract class Trie
-
- case class Nodo(car: Char, marcada: Boolean, hijos: List[Trie]) extends Trie
-
- case class Hoja(car: Char, marcada: Boolean) extends Trie
-
- def raiz(t: Trie): Char = {
-  t match {
-   case Nodo(c, _, _) => c
-   case Hoja(c, _) => c
-  }
- }
-
- def cabezas(t: Trie): Seq[Char] = {
-  t match {
-   case Nodo(_, _, hijos) => hijos.map(t => raiz(t))
-   case Hoja(c, _) => Seq(c)
-  }
- }
-
- def pertenece(s: Seq[Char], t: Trie): Boolean = {
-  s match {
-   case Seq() => true
-   case head +: tail =>
-    t match {
-     case Nodo(c, _, hijos) if c == head =>
-      hijos.exists(h => pertenece(tail, h))
-     case _ => false
-    }
-  }
- }
 /**
- def adicionar(s: Seq[Char], t: Trie): Trie = {
-  s match {
-   case Seq() => t // No se puede añadir una cadena vacía
-   case c +: cs =>
-    t match {
-     case Nodo(r, m, hijos) =>
-      val nuevoHijo = hijos.find(hijo => raiz(hijo) == c) match {
-       case Some(hijoExistente) => adicionar(cs, hijoExistente)
-       case None => Nodo(c, false, Seq()) // Crear un nuevo hijo
-      }
-      Nodo(r, m, hijos.filter(hijo => raiz(hijo) != c) :+ nuevoHijo)
-     case _ => Nodo(c, false, Seq(adicionar(cs, Hoja(c, true))))
-    }
-  }
- }
 
- def arbolDeSufijos(ss: Seq[Seq[Char]]): Trie = {
-  val trieVacio = Nodo(' ', false, Seq())
-  ss.foldLeft(trieVacio)((t, s) => adicionar(s :+ '$', t)) // Agregar el delimitador '$'
- }
-
- def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
-  true
- }
-
- def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
-  true
- }
-
- def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
-  true
- }
-
- def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
-  true
- }
-
- def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
-  true
- }
-
- def reconstruirCadenaIngenuoPar (umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
- true
- }
- def reconstruirCadenaMejoradoPar (umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
- true
- }
- def reconstruirCadenaTurboPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
- true
- }
- def reconstruirCadenaTurboMejoradaPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
- true
- }
- def reconstruirCadenaTurboAceleradaPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
- true
- }
+Taller 3 - Programación Funcional
+Autores: <Estudiantes>
+Profesor: Carlos A Delgado
  */
+package Proyecto
 
+import org.scalameter.measure
+import org.scalameter.withWarmer
+import org.scalameter.Warmer
+
+object proyecto{
 
  def main(args: Array[String]): Unit = {
 
-  type Oraculo = Seq[Char] => Boolean
+  val s: LazyList[String] = LazyList("A","C","T","G")
+  //println(PRC_ingenuo(s,13,oraculo))
+  val w = PRCingenuo(s,3,oraculo)
+  for (z <- w) println(z)
 
-  def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
-   def generarCombinaciones(n: Int, alfabeto: Seq[Char]): Seq[Seq[Char]] = {
-    if (n == 0) Seq(Seq.empty)
-    else {
-     for {
-      letra <- alfabeto
-      combinacion <- generarCombinaciones(n - 1, alfabeto)
-     } yield letra +: combinacion
-    }
-   }
-   generarCombinaciones(n, alfabeto).find(o).getOrElse(Seq.empty)
+
+ }
+
+ def oraculo(sub: String,cad: String): Boolean = {
+  cad.contains(sub)
+ }
+
+ def cerradura(l: LazyList[String], n: Int): LazyList[String] = {
+  require(n >= 0)
+  def generarCerraduraKleene(c: LazyList[String], m: Int): LazyList[String] = {
+   if (m > n) LazyList.empty
+   else c #::: generarCerraduraKleene(c.flatMap(c => c.map(c + )), m + 1)
   }
-  val oraculoEjemplo: Oraculo = {
-   case Seq('a','g','g','a') => true
-   case _ => false
-  }
-  val cadenaReconstruida = reconstruirCadenaIngenuo(4, oraculoEjemplo)
-  println(cadenaReconstruida)
+
+  generarCerraduraKleene(l, 1)
+ }
+
+
+ def PRCingenuo(L: LazyList[String],n: Int,f:(String,String) => Boolean): LazyList[String] = {
+  require(n > 0)
+  val s = cerradura(L,n)
+  val g = s.takeWhile(f(,"AAA"))
+  g
  }
 }
