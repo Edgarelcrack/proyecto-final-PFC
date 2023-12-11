67120a8d0d4d9b6ff479f87ec13f4f8b065d31db
JuanPE-PNG <juan.escobar.viveros@correounivalle.edu.co>
2023-12-11 18:02:32 -0500

    PRCIngenuo

--- app/src/main/scala/Proyecto/Proyecto.scala
+++ app/src/main/scala/Proyecto/Proyecto.scala
@@ -10,37 +10,32 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 
-object proyecto{
+object Proyecto{
 
  def main(args: Array[String]): Unit = {
-
-  val s: LazyList[String] = LazyList("A","C","T","G")
-  //println(PRC_ingenuo(s,13,oraculo))
-  val w = PRCingenuo(s,3,oraculo)
-  for (z <- w) println(z)
-
-
+  val x = List("C", "T", "A", "A", "C")
+  val w = PRCingenuo(x, x.length, oraculo)
+  println(w)
  }
 
- def oraculo(sub: String,cad: String): Boolean = {
+
+ def oraculo(sub: String, cad: String): Boolean = {
   cad.contains(sub)
  }
 
- def cerradura(l: LazyList[String], n: Int): LazyList[String] = {
-  require(n >= 0)
-  def generarCerraduraKleene(c: LazyList[String], m: Int): LazyList[String] = {
-   if (m > n) LazyList.empty
-   else c #::: generarCerraduraKleene(c.flatMap(c => c.map(c + )), m + 1)
+ def PRCingenuo(L: List[String], n: Int, f: (String, String) => Boolean): List[String] = {
+  require(n > 0)
+
+  def cerraduraIngenua(l: List[String], n: Int): List[String] = {
+   def generarCerraduraKleene(c: List[String], m: Int): List[String] = {
+    if (m > n) List.empty
+    else c ::: generarCerraduraKleene(c.flatMap(c => l.map(c + _)), m + 1)
+   }
+   generarCerraduraKleene(l, n)
   }
 
-  generarCerraduraKleene(l, 1)
- }
-
-
- def PRCingenuo(L: LazyList[String],n: Int,f:(String,String) => Boolean): LazyList[String] = {
-  require(n > 0)
-  val s = cerradura(L,n)
-  val g = s.takeWhile(f(,"AAA"))
+  val s = cerraduraIngenua(L, n)
+  val g = s.takeWhile(f(_, (L.toString())))
   g
  }
 }
