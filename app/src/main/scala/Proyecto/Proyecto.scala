46e50df5e0f576aa81ede83b885216b681cc490c
Edgar Andres <edgar.vargas@correounivalle.edu.co>
2023-12-12 20:45:25 -0500

    POWER

--- app/src/main/scala/Proyecto/Proyecto.scala
+++ app/src/main/scala/Proyecto/Proyecto.scala
@@ -15,42 +15,55 @@ object proyecto {
   val alfabeto = Seq('A','C','G','T')
   type Oraculo = Seq[Char] => Boolean
 
+
+
   def main(args: Array[String]): Unit = {
-    val oraculo: Oraculo = (secuencia: Seq[Char]) => secuencia.mkString == "ACGT"
-    val resultado = reconstruirCadenaIngenuo(4, oraculo)
-    println(resultado)
+    val secuancia = Seq('A','G','G','A')
+    val oraculo: Oraculo = (s: Seq[Char]) => {
+      secuancia.containsSlice(s)
+    }
+    println(reconstruirCadenaIngenuo(4, oraculo))
+    println(reconstruirCadenaMejorado(4,oraculo))
+    println(reconstruirCadenaTurbo(4,oraculo))
+
   }
 
-  def generarCerraduraKleeneIngenua(alfabeto: Seq[Char], longitud: Int): Seq[String] = {
-    def generarRecursivo(cadenaParcial: String, n: Int): Seq[String] = {
+  def reconstruirCadenaIngenuo(n: Int, oraculo: Oraculo): Seq[Char] = {
+    def generarRecursivo(cadenaParcial: Seq[Char], n: Int): Seq[Seq[Char]] = {
       if (n == 0) Seq(cadenaParcial)
       else alfabeto.flatMap { caracter =>
-        generarRecursivo(cadenaParcial + caracter, n - 1)
+        generarRecursivo(cadenaParcial :+ caracter, n - 1)
       }
     }
-
-    generarRecursivo("", longitud)
-  }
-
-  def generarCerraduraKleene(alfabeto: Seq[Char], longitud: Int): Seq[String] = {
-    def generarRecursivo(cadenaParcial: String, n: Int): Seq[String] = {
-      if (n == 0) Seq(cadenaParcial)
-      else {
-        val subcadenas = alfabeto.flatMap { caracter =>
-          generarRecursivo(cadenaParcial + caracter, n - 1)
-        }
-        Seq(cadenaParcial) ++ subcadenas
-      }
-    }
-
-    (1 to longitud).flatMap { n =>
-      generarRecursivo("", n)
-    }
-  }
-  def reconstruirCadenaIngenuo(n:Int, oraculo: Oraculo): Option[String] = {
-    val cerradura = generarCerraduraKleeneIngenua(alfabeto, n)
+    val cerradura = generarRecursivo(Seq.empty[Char], n)
     cerradura.find { secuencia =>
-      oraculo(secuencia.toSeq)
-    }
+      oraculo(secuencia)
+    }.getOrElse(List.empty[Char])
   }
+
+
+  def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
+    def generarCadenas(k: Int, sc: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k > n) sc
+      else { val sck = sc.flatMap(s => alfabeto.map(a => s :+ a)).filter(o);
+        generarCadenas(k + 1, sck);
+      }
+    }
+
+    val sc = generarCadenas(1, Seq(Seq()));
+    sc.find(_.length == n).getOrElse(Seq());
+  }
+
+  def reconstruirCadenaMejorado2(n: Int, o: Oraculo): Seq[Char] = {
+    def generarCadenas(k: Int, sc: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      if (k > n) sc
+      else { val sck = sc.flatMap(s => alfabeto.map(a => s :+ a)).filter(o)
+        generarCadenas(k + 1, sck)
+      }
+    }
+    val sc = generarCadenas(1, Seq(Seq.empty[Char]))
+    sc.reduce((s1, s2) => s1 ++ s2).take(n)
+  }
+
+
 }
